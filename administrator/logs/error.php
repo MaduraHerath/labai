8b90cdef967e803f4fcef78affddad2bd371daa2
MaduraHerath <maduraherath8@gmail.com>
2016-11-19 02:40:43 +0530

    fabric

---
+++ administrator/logs/error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-18 19:30:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-18T19:30:45+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:30:56+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:46:54+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:47:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:47:24+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:47:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T19:47:27+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
